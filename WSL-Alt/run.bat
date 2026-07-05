6df1db3066b4a1728b9cc64258fc221f0af13a24
Codeblub <codeblub@gmail.com>
2026-07-05 18:08:31 +0000

    built the kernel and debian image and compiled the app

---
+++ WSL-Alt/run.bat
@@ -0,0 +1,7 @@
+@echo off
+.\qemu\qemu-system-x86_64.exe ^
+    -kernel linux\arch\x86\boot\bzImage ^
+    -initrd initramfs.cpio.gz ^
+    -append "console=ttyS0 root=/dev/ram" ^
+    -nographic
+pause
