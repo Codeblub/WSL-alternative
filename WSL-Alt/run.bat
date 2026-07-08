df2e95c9a555163432f9c3fd9af8bcbf78b23dd5
Codeblub <codeblub@gmail.com>
2026-07-08 15:48:36 +0000

    lfs

--- WSL-Alt/run.bat
+++ WSL-Alt/run.bat
@@ -1,7 +1,7 @@
 @echo off
 title WSL-Alt
 .\qemu\qemu-system-x86_64.exe ^
-    -kernel OS\arch\x86\boot\bzImage ^
+    -kernel Linux\arch\x86\boot\bzImage ^
     -initrd initramfs.cpio.gz ^
     -append "console=ttyS0 root=/dev/ram" ^
     -nographic
