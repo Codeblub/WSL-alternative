33b6f9fec71a9fcb9c290ad90ce953032b8f1554
Codeblub <codeblub@gmail.com>
2026-07-09 18:03:30 +0000

    Edit batch so it has storage perms

--- WSL-Alt/run.bat
+++ WSL-Alt/run.bat
@@ -3,6 +3,7 @@ title WSL-Alt
 .\qemu\qemu-system-x86_64.exe ^
     -kernel Linux\arch\x86\boot\bzImage ^
     -initrd initramfs.cpio.gz ^
-    -append "console=ttyS0 root=/dev/ram" ^
+    -hda my_virtual_disk.img ^
+    -append "console=ttyS0 root=/dev/sda rw" ^
     -nographic
 pause
